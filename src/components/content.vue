7b7045da0d437ed4896314edf5480cef94805ffb
francisco <franzamora9@outlook.com>
2019-06-26 18:37:01 -0300

    primer commit

---
+++ src/components/content.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="content">
+    <h1>{{movieTitle}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['movieTitle'],
+  data() {
+    return {
+      movies: []
+    }
+  },
+  mounted() {
+    this.loadCatalog()
+  },
+  methods: {
+    loadCatalog: async function() {
+      const data = await fetch('./catalog.json')
+      this.movies = await data.json()
+    }
+  }
+}
+</script>
---
+++ src/components/navBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <nav class="navbar navbar-light bg-light">
+      <form class="form-inline" @submit.prevent="search">
+        <input v-model="titulo"
+          class="form-control mr-sm-2"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+        >
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'navBar',
+ data() {
+  return{
+        titulo:'',
+     }
+  },
+ methods: {
+   search(){
+     console.log('hola')
+      },
+    }
+}
+</script>
